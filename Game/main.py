2122b7b5cb302b5b3fc369bdd1ce29dbc52f52a0
BlackAlexander <acon3331@yahoo.com>
2021-09-03 00:29:46 +0300

    dice

--- Game/main.py
+++ Game/main.py
@@ -19,10 +19,6 @@ def play_game():
     screen = pygame.display.set_mode(size)
     background = pygame.image.load("../Images/Board.png")
 
-    # pieces
-    white_piece = pygame.image.load("../Images/Piece-White.png")
-    black_piece = pygame.image.load("../Images/Piece-Black.png")
-
     # game data
     table = default_table()
     playing = True
@@ -34,7 +30,7 @@ def play_game():
     pipfont = pygame.font.SysFont('Times New Roman', 50)
 
     while playing:
-        # mouse = pygame.mouse.get_pos()
+        mouse = pygame.mouse.get_pos()
         # click = pygame.mouse.get_pressed()
 
         # text
@@ -46,6 +42,7 @@ def play_game():
         screen.blit(blacksurface, (362, -7))
         screen.blit(whitesurface, (362, 752))
         put_pieces(screen, table)
+        put_dice(screen, 2, 3)
 
         pygame.display.update()
 
@@ -64,8 +61,6 @@ def default_table():
     to_return[16] = [b, b, b]
     to_return[18] = [b, b, b, b, b]
     to_return[23] = [w, w]
-    for i in to_return:
-        print(i)
     return to_return
 
 
@@ -80,16 +75,39 @@ def put_pieces(screen, table):
                 screen.blit(black_piece, get_piece_position(i, j))
 
 
+def get_dice():
+    value1 = random.randint(1, 6)
+    value2 = random.randint(1, 6)
+    return [value1, value2]
+
+
+def put_dice(screen, value1, value2):
+    # dice_first = rotated_image = pygame.transform.rotate(dice_2, 30)
+    dice_1 = pygame.image.load("../Images/Dice-1.png")
+    dice_2 = pygame.image.load("../Images/Dice-2.png")
+    dice_3 = pygame.image.load("../Images/Dice-3.png")
+    dice_4 = pygame.image.load("../Images/Dice-4.png")
+    dice_5 = pygame.image.load("../Images/Dice-5.png")
+    dice_6 = pygame.image.load("../Images/Dice-6.png")
+    dice_s = pygame.image.load("../Images/Dice-Shadow.png")
+    dices = [dice_s, dice_1, dice_2, dice_3, dice_4, dice_5, dice_6]
+    first_dice = dices[value1]
+    second_dice = dices[value2]
+    screen.blit(dice_s, (158, 373))
+    screen.blit(first_dice, (148, 363))
+    screen.blit(dice_s, (237, 403))
+    screen.blit(second_dice, (227, 393))
+
 
 def get_piece_position(row, height):
-    piece_x = 700
-    piece_y = 40
+    # piece_x = 700
+    # piece_y = 40
     x_positions = [700, 644, 588, 532, 476, 420, 320, 264, 208, 152, 96, 40, 40, 96, 152, 208, 264, 320, 420, 476, 532, 588, 644, 700]
     piece_x = x_positions[row]
     if row <= 11:
-        piece_y = 35 + (height) * 52
+        piece_y = 35 + height * 52
     else:
-        piece_y = 703 - (height) * 52
+        piece_y = 703 - height * 52
     return (piece_x, piece_y)
 
 
